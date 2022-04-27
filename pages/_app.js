d4fdf3f5a52fc46d5985a132c3c9a0f3aae61ad6
KuroMuta <legiony530@gmail.com>
2022-04-27 21:22:35 +0700

    web3

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
-
+import { MoralisProvider } from "react-moralis";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider serverUrl="https://ftfl216f4tqt.usemoralis.com:2053/server" appId="f67bySASMxzCqGLStPKQpIAlZru9eTAVaw9tn2O8">
+      <Component {...pageProps} />
+    </MoralisProvider>
+    )
 }
 
 export default MyApp
